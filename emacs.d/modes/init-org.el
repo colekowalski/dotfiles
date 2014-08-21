48a8d61151b2d2947308a81f618df32e831ff516
Cole Kowalski <cole@northernbloc.org>
2014-08-21 12:12:55 -0400

    [emacs] better capture and refiling

--- emacs.d/modes/init-org.el
+++ emacs.d/modes/init-org.el
@@ -7,9 +7,20 @@
 
 (setq org-agenda-files `(,org-directory))
 
-(setq org-startup-indented t)
+;; default file used in capture templates
+(setq org-default-notes-file
+      (concat
+       (file-name-as-directory org-directory)
+       "inbox.org"))
+
+;; allow refiling between org files included in org-agenda files
+(setq org-refile-targets '((nil :maxlevel . 3)
+                           (org-agenda-files :maxlevel . 3)))
+
+(setq org-startup-indented nil)
 
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key "\C-cc" 'org-capture)
 
 (provide 'init-org)
