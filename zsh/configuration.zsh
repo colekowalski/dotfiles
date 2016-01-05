f0195b4bacc264c549f9c3c01120e25faf669dba
Cole Kowalski <cole@northernbloc.org>
2016-01-05 11:12:07 -0500

    use base16 version of solarized

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -7,6 +7,10 @@ export GIT_EDITOR='vim'
 # Enable more syntax highlighting
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 
+# Load base16 shell configuration
+BASE16_SHELL="$DOTFILES/zsh/vendor/base16-shell/base16-solarized.dark.sh"
+[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
+
 if [[ ! "$OSTYPE" = darwin* ]]; then
   eval $(dircolors $DOTFILES/zsh/vendor/dircolors-solarized/dircolors.ansi-dark)
 fi
