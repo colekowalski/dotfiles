6be097e8ded3ac7350ed9b364b4fc8e3d6a9aaf8
Cole Kowalski <cole@northernbloc.org>
2016-01-19 21:40:00 -0500

    [zsh] set the editor in the local configuration

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -1,9 +1,6 @@
 ## make sure local bin dir is on the path
 [ -d $HOME/bin ] && export PATH=$HOME/bin:$PATH
 
-export EDITOR='vim'
-export GIT_EDITOR='vim'
-
 # Enable more syntax highlighting
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 
