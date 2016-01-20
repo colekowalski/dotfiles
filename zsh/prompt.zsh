ded755e7773021bed1ec6f175e8fa2dc6748d6c5
Cole Kowalski <cole@northernbloc.org>
2016-01-19 21:39:09 -0500

    [zsh] jumping between vi mode and emacs mode on remote servers was driving me crazy

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -10,9 +10,5 @@ function __git_branch {
     echo " (${branch#refs/heads/}) "
 }
 
-
 PROMPT_STATUS=%(?..%{$fg[red]%}ಠ_ಠ )%{$reset_color%}
 PROMPT='${PROMPT_STATUS}[%{$fg[blue]%}%~%{$reset_color%}]$(__git_branch)%# '
-
-MODE_INDICATOR="%{$fg[yellow]%}[n]%{$reset_color%} "
-
