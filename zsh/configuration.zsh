92b47de9fcb4c8d74bcc11fa80c9120c09d9586b
Cole Kowalski <cole@northernbloc.org>
2016-07-06 20:13:21 -0400

    [zsh] remove dircolors-solarized

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -7,7 +7,3 @@ ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 # Load base16 shell configuration
 BASE16_SHELL="$DOTFILES/zsh/vendor/base16-shell/base16-solarized.dark.sh"
 [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
-
-if [[ ! "$OSTYPE" = darwin* ]]; then
-  eval $(dircolors $DOTFILES/zsh/vendor/dircolors-solarized/dircolors.ansi-dark)
-fi
