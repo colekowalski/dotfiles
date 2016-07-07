d301c1d4ab25992635add48896edf94b670b6b0c
Cole Kowalski <cole@northernbloc.org>
2016-07-06 21:43:45 -0400

    update base16

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -5,5 +5,5 @@
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 
 # Load base16 shell configuration
-BASE16_SHELL="$DOTFILES/zsh/vendor/base16-shell/base16-solarized.dark.sh"
+BASE16_SHELL="$DOTFILES/zsh/vendor/base16-shell/scripts/base16-solarized-dark.sh"
 [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
