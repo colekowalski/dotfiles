38b1b60b160f314e9c7eebf29f1ec069e3b48726
Cole Kowalski <cole@northernbloc.org>
2017-01-31 11:24:15 -0500

    [zsh] bash-style word splitting

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -7,3 +7,7 @@ ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 # Load base16 shell configuration
 BASE16_SHELL="$DOTFILES/zsh/vendor/base16-shell/scripts/base16-solarized-dark.sh"
 [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
+
+## make (backward|forward)-word behave like bash
+autoload -U select-word-style
+select-word-style bash
