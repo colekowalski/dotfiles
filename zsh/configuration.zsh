8cbdfb04347508b675c60e317ee3d43c84bac37a
Cole Kowalski <cole@northernbloc.org>
2017-03-21 12:45:11 -0400

    Time for a theme change

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -5,7 +5,7 @@
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 
 # Load base16 shell configuration
-BASE16_SHELL="$DOTFILES/zsh/vendor/base16-shell/scripts/base16-solarized-dark.sh"
+BASE16_SHELL="$DOTFILES/zsh/vendor/base16-shell/scripts/base16-chalk.sh"
 [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
 
 ## make (backward|forward)-word behave like bash
