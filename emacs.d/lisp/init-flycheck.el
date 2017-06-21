7adeff7016c363b60d9bf8239dd607aa9817599a
Cole Kowalski <cole@northernbloc.org>
2017-06-20 21:52:42 -0400

    Updates

--- emacs.d/lisp/init-flycheck.el
+++
@@ -1,6 +0,0 @@
-(require 'flycheck)
-(require 'flycheck-pyflakes)
-
-(add-hook 'python-mode-hook 'flycheck-mode)
-
-(provide 'init-flycheck)
--- emacs.d/lisp/init-theme.el
+++
@@ -1,4 +0,0 @@
-(when (fboundp 'load-theme)
-  (load-theme 'wombat t))
-
-(provide 'init-theme)
