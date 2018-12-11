6d9148e02506b695bf45114d80cc8229e23497b3
Cole Kowalski <cole@northernbloc.org>
2018-12-11 12:32:51 -0500

    Ivy updates

--- emacs/.emacs.d/modules/init-ivy.el
+++ emacs/.emacs.d/modules/init-ivy.el
@@ -1,14 +1,14 @@
 (use-package ivy
   :ensure t
-  :init
-  (ivy-mode 1))
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "[%d/%d]")
+  (ivy-mode t))
 
 (use-package counsel
   :ensure t
-  :after ivy)
-
-(use-package swiper
-  :ensure t
-  :after ivy)
+  :after ivy
+  :bind (("C-c g" . counsel-git)
+	 ("C-c k" . counsel-ag)))
 
 (provide 'init-ivy)
