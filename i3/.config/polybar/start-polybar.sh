a1aaaa1e5b92ba3b07a3bfce9d2976a5004d3349
Cole Kowalski <cole@northernbloc.org>
2019-04-08 16:09:03 -0400

    Add polybar configuration

---
+++ i3/.config/polybar/start-polybar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if pgrep -u $UID -x polybar > /dev/null; then
+  killall -q -u "${USER}" polybar
+
+  count=0
+  while pgrep -u "${UID}" -x polybar > /dev/null; do
+    # if polybar has taken more than 5 seconds to shutdown, take more drastic
+    # action
+    if [ "${count}" > 5 ]; then
+      killall -q -u "${USER}" -9 polybar
+      break
+    fi
+
+    count=$count + 1
+    sleep 1
+  done
+fi
+
+echo "Launching polybar..."
+polybar -c "${HOME}/.config/polybar/config" primary &
+polybar -c "${HOME}/.config/polybar/config" secondary &
