59965f372984b9b27e7b06576ff92dcba79a4f79
Cole Kowalski <cole@northernbloc.org>
2019-04-11 11:12:10 -0400

    Update mutt config
    * Only show folders with new mail in the sidebar.
    
    * Ignore trash directories in the sidebar mailbox list

--- email/.mutt/mailboxes.py
+++ email/.mutt/mailboxes.py
@@ -3,12 +3,21 @@
 from __future__ import print_function
 
 import os
-import re
-import sys
-
 
 TOPDIR = '/home/cole/.mail/'
+
+# The presence of these subdirectories within a directory indicates
+# that the directory is a maildir
 IMAP_DIRS = set(['cur', 'new', 'tmp'])
+
+# Do not include these mailboxes in the mailbox list. Only the folder
+# name is checked so ignores are global and applied across all
+# accounts
+IGNORED_MAILBOXES = [
+    'Trash',
+    'Deleted Items',
+]
+
 
 def get_subdirs(path):
     dirs = []
@@ -24,13 +33,26 @@ def is_maildir(subdirs):
     return 'cur' in [os.path.basename(d) for d in subdirs]
 
 
+def is_ignored(mailbox):
+    path = os.path.normpath(mailbox)
+    parts = path.split(os.sep)
+    return parts[-1] in IGNORED_MAILBOXES
+
+
 def get_mailboxes(path):
     mailboxes = []
     subdirs = get_subdirs(path)
     if len(subdirs) < 1:
         return mailboxes
 
+    # Ignore both the maildir itself as well as any subfolders if the
+    # maildir is in the ignored list
+    if is_ignored(path):
+        return mailboxes
+
     for subdir in [d for d in subdirs if os.path.basename(d) not in IMAP_DIRS]:
+        if is_ignored(subdir):
+            continue
         mailboxes += get_mailboxes(subdir)
 
     if is_maildir(subdirs) or len(mailboxes) > 0:
@@ -41,8 +63,6 @@ def get_mailboxes(path):
 
 def escape_path(path):
     path = path.replace(' ', '\\ ')
-    #path = path.replace('[', '\\[')
-    #path = path.replace(']', '\\]')
     return path
 
 
@@ -51,6 +71,5 @@ for acct in get_subdirs(TOPDIR):
     if os.path.basename(acct) in set(['grooveshark']):
         continue
     mailboxes += get_mailboxes(acct)
-#mailboxes = get_mailboxes(TOPDIR)
 mailboxes = [escape_path(x) for x in mailboxes]
 print(' '.join(sorted(mailboxes)))
