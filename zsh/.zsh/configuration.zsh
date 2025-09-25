0432f80fb058cb7dd86bdb6c9d0dbf83c0b38897
Cole Kowalski <cole@northernbloc.org>
2025-09-25 15:06:58 -0400

    Remove base16-shell

--- zsh/.zsh/configuration.zsh
+++ zsh/.zsh/configuration.zsh
@@ -1,11 +1,6 @@
 ## make sure local bin dir is on the path
 [ -d $HOME/.bin ] && export PATH=$HOME/.bin:$PATH
-
-# Load base16 shell configuration
-# if ! remote_connection; then
-#   BASE16_SHELL="$HOME/.zsh/vendor/base16-shell/scripts/base16-tomorrow-night.sh"
-#   [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
-# fi
+[ -d $HOME/.local/bin ] && export PATH=$HOME/.local/bin:$PATH
 
 ## make (backward|forward)-word behave like bash
 autoload -U select-word-style
