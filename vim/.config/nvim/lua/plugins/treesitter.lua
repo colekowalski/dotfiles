df7bfdb147a4f302f323648366b4008db3a4ea45
Cole Kowalski <cole@northernbloc.org>
2026-01-28 11:19:43 -0500

    [vim] Bump plugins and fix treesitter configuration

--- vim/.config/nvim/lua/plugins/treesitter.lua
+++ vim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,35 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-        ensure_installed = {
-          "c",
-          "cmake",
-          "cpp",
-          "css",
-          "gitignore",
-          "go",
-          "html",
-          "http",
-          "java",
-          "javascript",
-          "lua",
-          "rust",
-          "scss",
-          "sql",
-          "typescript",
-          "vim",
-          "vimdoc"
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },  
-      })
-    end
-  }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "c",
+                    "cmake",
+                    "cpp",
+                    "css",
+                    "gitignore",
+                    "go",
+                    "html",
+                    "http",
+                    "java",
+                    "javascript",
+                    "lua",
+                    "rust",
+                    "scss",
+                    "sql",
+                    "typescript",
+                    "vim",
+                    "vimdoc",
+                },
+                highlight = {
+                    enable = true,
+                },
+                indent = {
+                    enable = true,
+                },
+            })
+        end,
+    },
 }
