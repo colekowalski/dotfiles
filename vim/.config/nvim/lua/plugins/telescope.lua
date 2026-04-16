cc65996b375594753dc0a311e21534def9010cbd
Cole Kowalski <cole@northernbloc.org>
2026-04-15 22:07:04 -0400

    Neovim config updates

--- vim/.config/nvim/lua/plugins/telescope.lua
+++ vim/.config/nvim/lua/plugins/telescope.lua
@@ -2,14 +2,42 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'zschreur/telescope-jj.nvim',
+    },
+    config = function()
+      local telescope = require("telescope")
+      pcall(telescope.load_extension, "fzf")
+      pcall(telescope.load_extension, "jj")
+    end,
     keys = {
-      {"<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files"},
-      {"<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep"},
-      {"<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers"},
-      {"<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Telescope help tags"},
-      {"<leader>fr", "<cmd>Telescope registers<cr>", desc = "Telescope registers"},
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Telescope live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Telescope buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Telescope help tags" },
+      { "<leader>fr", "<cmd>Telescope registers<cr>",  desc = "Telescope registers" },
+
+      {
+        "<leader>sf",
+        function()
+          -- Try jj first
+          local jj_ok = pcall(require("telescope").extensions.jj.files)
+          if jj_ok then
+            return
+          end
+          -- Fallback to find_files (works everywhere)
+          require("telescope.builtin").find_files({ hidden = true })
+        end,
+        desc = "Find files (jj/git aware)",
+      },
+      {
+        "<leader>sj",
+        function()
+          require("telescope").extensions.jj.diff()
+        end,
+        desc = "jj diff (changed files)",
+      },
     }
   }
 }
-
--- vim/.config/nvim/lua/plugins/treesitter.lua
+++ vim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,42 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "c",
-                    "cmake",
-                    "cpp",
-                    "css",
-                    "gitignore",
-                    "go",
-                    "html",
-                    "http",
-                    "java",
-                    "javascript",
-                    "lua",
-                    "rust",
-                    "scss",
-                    "sql",
-                    "typescript",
-                    "vim",
-                    "vimdoc",
-                },
-                highlight = {
-                    enable = true,
-                },
-                indent = {
-                    enable = true,
-                },
-            })
-        end,
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "bash",
+          "c",
+          "cmake",
+          "cpp",
+          "css",
+          "gitignore",
+          "go",
+          "html",
+          "http",
+          "java",
+          "javascript",
+          "json",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "rust",
+          "scss",
+          "sql",
+          "terraform",
+          "toml",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "yaml",
+        },
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
 }
