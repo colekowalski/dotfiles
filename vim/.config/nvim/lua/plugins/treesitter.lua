a51f1130514ae315f3a7795f2aabadd6b5c13302
Cole Kowalski <cole@northernbloc.org>
2026-05-14 15:50:32 -0400

    feat(nvim): Update Treesitter config

--- vim/.config/nvim/lua/plugins/treesitter.lua
+++ vim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,41 +1,44 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "cmake",
-          "cpp",
-          "css",
-          "gitignore",
-          "go",
-          "html",
-          "http",
-          "java",
-          "javascript",
-          "json",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "rust",
-          "scss",
-          "sql",
-          "terraform",
-          "toml",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "yaml",
-        },
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
+      local treesitter = require("nvim-treesitter")
+      treesitter.setup()
+      treesitter.install({
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "css",
+        "gitignore",
+        "go",
+        "html",
+        "http",
+        "java",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "rust",
+        "scss",
+        "sql",
+        "terraform",
+        "toml",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local ok = pcall(vim.treesitter.start, args.buf)
+          if ok then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
       })
     end,
   },
